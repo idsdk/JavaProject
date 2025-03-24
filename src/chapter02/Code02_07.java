a40afe2cf4e9d2ddf5d8db0446fe846b4d3659d3
idsdk <idsdkk@gmail.com>
2025-03-24 10:45:15 +0900

    사칙계산기

---
+++ src/chapter02/Code02_07.java
@@ -0,0 +1,28 @@
+package chapter02;
+
+public class Code02_07 {
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 50;
+
+        result = num1 + num2;
+        System.out.printf("%d + %d = %d\n", num1, num2, result);
+
+        result = num1 - num2;
+        System.out.printf("%d - %d = %d\n", num1, num2, result);
+
+        result = num1 * num2;
+        System.out.printf("%d * %d = %d\n", num1, num2, result);
+
+        result = num1 / num2;
+        System.out.printf("%d / %d = %d\n", num1, num2, result);
+
+        result = num1 * num2;
+        System.out.printf("%d × %d = %d\n", num1, num2, result);
+
+        result = num1 / num2;
+        System.out.printf("%d ÷ %d = %d\n", num1, num2, result);
+    }
+}
