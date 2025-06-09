4db9638872a0edd875deda5e27f8e613a3491304
idsdk <idsdkk@gmail.com>
2025-06-09 11:48:31 +0900

    클래스와 객체 생성자

---
+++ src/chapter07/access/Dice.java
@@ -0,0 +1,15 @@
+package chapter07.access;
+
+public class Dice {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+}
--- src/chapter07/access/RabbitTest.java
+++ src/chapter07/access/RabbitTest.java
@@ -7,5 +7,8 @@ public class RabbitTest {
         r1.setPosition(50, 100);
         System.out.println("토끼 모양: " + r1.getShape());
         System.out.printf("토끼 현재 위치: (" + r1.getX() + ", " + r1.getY() + ")");
+
+        System.out.printf("토끼 모양은 [%s]입니다.", r1.getShape());
+        System.out.printf("토끼 위치는 ( %d , %d)입니다.", r1.getX(), r1.getY());
     }
 }
